19e979917c7d1199cc90dd222892de7e501dfc10
ratishtimalsina66 <ratishtimalsina66@gmail.com>
2026-07-09 03:01:41 -0400

    Remove router for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,15 @@
-import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
-import NotFound from "@/pages/not-found";
-
-function AppRoutes() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router base="/ratishportfolio">
-          <AppRoutes />
-        </Router>
+        <Home />
       </TooltipProvider>
     </QueryClientProvider>
   );
